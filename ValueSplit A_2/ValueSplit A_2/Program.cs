56c783d58ca72e50789ebafecbbfaf741acd9d5a
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-08-20 15:09:57 +0530

    Added Value Split prog.

---
+++ ValueSplit A_2/ValueSplit A_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ValueSplit_A_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rem = new int[5];
+            int n = 4;
+            int a = Convert.ToInt32(Console.ReadLine());
+            while(a != 0)
+            {
+                rem[n] = a % 10;
+                a = a / 10;
+                n--;
+            }
+            foreach(int i in rem)
+            {
+                Console.Write(i + "   ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
