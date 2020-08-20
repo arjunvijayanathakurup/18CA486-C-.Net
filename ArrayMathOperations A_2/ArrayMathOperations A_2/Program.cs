e3a8b6e4004dfa66c2e64da188b887ad443e62dd
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-08-20 14:19:42 +0530

    Added Array Math operations

---
+++ ArrayMathOperations A_2/ArrayMathOperations A_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ArrayMathOperations_A_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[3];
+            int sum = 0, smallest = 0, largest = 0, product = 1;
+            Console.WriteLine("Input three different integers: ");
+            for(int i = 0; i < 3; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+                sum += arr[i];
+                product *= arr[i];
+                if(i == 0)
+                {
+                    smallest = arr[i];
+                    largest = arr[i];
+                }
+                else if(arr[i] < smallest)
+                {
+                    smallest = arr[i];
+                }
+                if(arr[i] > largest)
+                {
+                    largest = arr[i];
+                }
+            }
+            Console.WriteLine("Sum is : " + sum);
+            Console.WriteLine("Average is :" + sum / arr.Length);
+            Console.WriteLine("Product is : " + product);
+            Console.WriteLine("Largest Value is: " + largest);
+            Console.WriteLine("Smallest Value is: " + smallest);
+            Console.ReadLine();
+        }
+    }
+}
