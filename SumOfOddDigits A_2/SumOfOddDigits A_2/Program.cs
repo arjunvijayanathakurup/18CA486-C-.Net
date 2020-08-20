cb3ab156e2a9d91e97d1aaa09210b47a938c51fd
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-08-20 15:17:52 +0530

    Added Sum of odd digits prog.

---
+++ SumOfOddDigits A_2/SumOfOddDigits A_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SumOfOddDigits_A_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int[] rem = new int[4];
+            int n = 3, sum = 0;
+            while (a != 0)
+            {
+                rem[n] = a % 10;
+                a = a / 10;
+                n--;
+            }
+            foreach(int i in rem)
+            {
+                if(i % 2 != 0 || i == 1)
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+        }
+    }
+}
