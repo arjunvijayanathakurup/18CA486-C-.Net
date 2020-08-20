b4a8b62b14829f8693369e990ebf49a77f4686ff
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-08-20 15:01:17 +0530

    Added Multiple of Second value prog.

---
+++ MultipleOfSecond A_2/MultipleOfSecond A_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MultipleOfSecond_A_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            if(a % b == 0)
+            {
+                Console.WriteLine(a + " is a multiple of " + b);
+            }
+            else
+            {
+                Console.WriteLine(a + " is a not multiple of " + b);
+            }
+            Console.ReadLine();
+        }
+    }
+}
