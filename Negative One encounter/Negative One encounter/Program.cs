6fa917c854bc2804eefa27c13fc9a3a966796300
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-08-27 15:04:35 +0530

    Added negative value program

---
+++ Negative One encounter/Negative One encounter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Negative_One_encounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, count_zero = 0, count_pos = 0, count_neg = 0;
+            while(a!= -1)
+            {
+                a = Convert.ToInt32(Console.ReadLine());
+                if(a == 0)
+                {
+                    count_zero += 1;
+                }else if(a > 1){
+                    count_pos += 1;
+                }
+                else if (a < 0)
+                {
+                    count_neg += 1;
+                }
+            }
+            Console.WriteLine("Count of negative numbers are:" + count_neg + "\n Count of Positive numbers are: " + count_pos + "\n Count of Zeros are: " + count_zero);
+            Console.ReadLine();
+        }
+    }
+}
