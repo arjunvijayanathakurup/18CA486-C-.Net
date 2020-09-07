67243716728fbcf90d2a24c38adbd818d5c38c57
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-09-07 07:13:05 +0530

    Added address passing using params

---
+++ Address A_4/Address A_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Address_A_4
+{
+    class Address
+    {
+        public void setAddress(params string[] str)
+        {
+            foreach(string st in str)
+            {
+                Console.WriteLine(st);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address address = new Address();
+            address.setAddress("Kadampattu House", "Ponakam", "Mavelikkara P.O", "Aleppy", "");
+            Console.ReadLine();
+        }
+    }
+}
