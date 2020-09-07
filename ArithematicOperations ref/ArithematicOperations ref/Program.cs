a1bb02091ad4cdaf2ecc2cf7098872b5cb8883af
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-09-07 07:26:27 +0530

    Added arithematic operations program using ref

---
+++ ArithematicOperations ref/ArithematicOperations ref/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace ArithematicOperations_ref
+{
+    class Number
+    {
+        int a;
+        int b;
+        int value;
+
+        public Number()
+        {
+            a = 0;
+            b = 0;
+            value = 0;
+        }
+        public void add(ref int a, ref int b)
+        {
+            value = a + b;
+            Console.WriteLine(a +  " + " + b + " = " + value);
+        }
+        public void sub(ref int a, ref int b)
+        {
+            value = a - b;
+            Console.WriteLine(a + " - " + b + " = " + value);
+        }
+        public void mul(ref int a, ref int b)
+        {
+            value = a * b;
+            Console.WriteLine(a + " x " + b + " = " + value);
+        }
+        public void div(ref int a, ref int b)
+        {
+            value = a / b;
+            Console.WriteLine(a + " / " + b + " = " + value);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 5;
+            Number number = new Number();
+            number.add(ref a, ref b);
+            number.sub(ref a, ref b);
+            number.mul(ref a, ref b);
+            number.div(ref a, ref b);
+            Console.ReadLine();
+        }
+    }
+}
