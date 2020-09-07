80587a4b82f5fb82c0ec7156b7f93bb68bef7221
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-09-07 06:58:39 +0530

    Added program using ref and out

---
+++ RefNumber A_4/RefNumber A_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RefNumber_A_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
