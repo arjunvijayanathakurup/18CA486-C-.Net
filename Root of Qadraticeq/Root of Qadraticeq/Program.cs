fe54df3f58b52305e45083e51a5a713b7f22ecec
Arjun Vijayanatha Kurup <arjunvijayanathakurup@gmail.com>
2020-09-20 18:53:43 +0530

    Added program for calculation of quadratic eq

---
+++ Root of Qadraticeq/Root of Qadraticeq/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Root_of_Qadraticeq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0, c = 0;
+            double d, x1, x2;
+            Console.WriteLine("Enter the values for a, b and c for the equation ax^2 + bx + c");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            d = (b * b) - (4 * a * c);
+
+            if(d == 0)
+            {
+                x1 = -b / (2 * a);
+                x2 = x1;
+                Console.WriteLine("Value 1: " + x1);
+                Console.WriteLine("Value 2: " + x2);
+            }
+            else if(d > 0)
+            {
+                x1 = Math.Abs(-b - Math.Sqrt(d)) / (2 * a);
+                x2 = Math.Abs(-b + Math.Sqrt(d)) / (2 * a);
+                Console.WriteLine("Value 1: " + x1);
+                Console.WriteLine("Value 2: " + x2);
+            }
+            else
+            {
+                Console.WriteLine("Cannot be calculated");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
